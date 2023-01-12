b9787468add224b30939b8d5bde2a66f6fdd82a5
Nachiket <Sarafnachiket@gmail.com>
2023-01-12 12:57:07 +0530

    Return cube of given array element in array.

---
+++ src/main/java/com/bootcoding/dsa/array/CubeOfArray.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa.array;
+
+public class CubeOfArray {
+    public static void main(String[] args) {
+        int[] array = {1,2,-3};
+        int[] cube = Cube(array);
+        for(int i =0; i<cube.length;i++){
+            System.out.println(cube[i]);
+        }
+
+        CubeOfArray cubeOfArray = new CubeOfArray();
+    }public static int[] Cube(int[] array){
+        int[] cube = new int[array.length];
+        for (int i = 0; i<array.length;i++){
+            cube[i] = array[i]*array[i]*array[i];
+
+        }
+        return cube;
+    }
+}
