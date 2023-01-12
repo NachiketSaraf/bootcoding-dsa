8e040bee68c38a2926658f8040f6b0f6c95e29b6
Nachiket <Sarafnachiket@gmail.com>
2023-01-12 12:59:38 +0530

    LeetCode problem 412 Fizz Buzz.

---
+++ src/main/java/com/bootcoding/dsa/leetcode/FizzBuzz.java
@@ -0,0 +1,37 @@
+package com.bootcoding.dsa.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        List<String> elements = fizzBuzz(10);
+        for(int i = 0; i<elements.size();i++ ){
+            System.out.println(elements.get(i));
+        }
+    }
+    public static List<String> fizzBuzz(int n) {
+
+        List<String> ans = new ArrayList<String>();
+
+        for (int num = 1; num <= n; num++) {
+
+            boolean divisibleBy3 = (num % 3 == 0);
+            boolean divisibleBy5 = (num % 5 == 0);
+
+            if (divisibleBy3 && divisibleBy5) {
+                ans.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                ans.add("Fizz");
+            } else if (divisibleBy5) {
+                ans.add("Buzz");
+            } else {
+                ans.add(Integer.toString(num));
+            }
+        }
+
+        return ans;
+    }
+
+}
