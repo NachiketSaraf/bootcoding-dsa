6e27e88b0131c8f2320cc9221118fd6f302c1194
Nachiket <Sarafnachiket@gmail.com>
2023-01-17 00:34:25 +0530

    Finding duplicate number in array

---
+++ src/main/java/com/bootcoding/dsa/array/DuplicateInArray.java
@@ -0,0 +1,18 @@
+package com.bootcoding.dsa.array;
+
+public class DuplicateInArray {
+    public static void main(String[] args) {
+
+        int[] num = {1,2, 7, 9,3, 15,9,1};
+
+        printDuplicate(num);
+    }
+
+    private static void printDuplicate(int[] num) {
+        for (int i = 0; i< num.length;i++){
+            for (int j = i;j< num.length;j++){
+
+            }
+        }
+    }
+}
