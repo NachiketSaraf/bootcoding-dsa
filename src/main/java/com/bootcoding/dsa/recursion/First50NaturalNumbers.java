3a26dc98d0d12cf5cba0af63f90c56e0f1c006fd
Nachiket <Sarafnachiket@gmail.com>
2023-01-18 00:29:16 +0530

    WAP to print first 50 natural numbers using recursion.

--- src/main/java/com/bootcoding/dsa/recursion/First50NaturalNumbers.java
+++ src/main/java/com/bootcoding/dsa/recursion/First50NaturalNumbers.java
@@ -1,5 +1,6 @@
 package com.bootcoding.dsa.recursion;
 
+//WAP to print first 50 natural numbers using recursion (1,2,3,4....)
 public class First50NaturalNumbers {
     public static void main(String[] args) {
         int num = 50;
