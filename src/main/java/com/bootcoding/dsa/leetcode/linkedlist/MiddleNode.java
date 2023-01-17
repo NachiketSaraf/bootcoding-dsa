932f5bdf5648aad8087c721e7855d5bed30cbf11
Nachiket <Sarafnachiket@gmail.com>
2023-01-18 00:27:09 +0530

    LeetCode problem 876 Middle of the Linked List.

---
+++ src/main/java/com/bootcoding/dsa/leetcode/linkedlist/MiddleNode.java
@@ -0,0 +1,47 @@
+package com.bootcoding.dsa.leetcode.linkedlist;
+
+import com.bootcoding.dsa.leetcode.linkedlist.ListNode;
+
+public class MiddleNode {
+    public static void main(String[] args) {
+        ListNode firstnode = new ListNode(1);
+        ListNode secondnode = new ListNode(2);
+        ListNode thirdnode = new ListNode(3);
+        ListNode forthnode = new ListNode(4);
+        ListNode fifthnode = new ListNode(5);
+
+        ListNode head = firstnode;
+        firstnode.next = secondnode;
+        secondnode.next = thirdnode;
+        thirdnode.next = forthnode;
+        forthnode.next = fifthnode;
+        fifthnode = null;
+       ListNode h= middleNode(head);
+       while (h!=null){
+           System.out.println(h.val);
+           h = h.next;
+       }
+
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int nodeCounter = 0;
+        ListNode temp2 = head;
+        int halfCounter;
+
+        while (temp != null) {
+            nodeCounter++;
+            temp = temp.next;
+        }
+        halfCounter = (nodeCounter) / 2;
+
+        for (int i = 0; i < halfCounter; i++) {
+            temp2 = temp2.next;
+        }
+        return temp2;
+
+    }
+
+
+}
