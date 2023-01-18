a572d132987e95b92553f2257cde9e6a9764fc19
Nachiket <Sarafnachiket@gmail.com>
2023-01-19 00:12:12 +0530

    WAP to sum of numbers from 1 to n using recursion

---
+++ src/main/java/com/bootcoding/dsa/recursion/SumOfNumbers1ToN.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa.recursion;
+
+public class SumOfNumbers1ToN {
+    public static void main(String[] args) {
+        int n = 5;
+
+        int s=printSum(n);
+        System.out.println(s);
+
+    }
+
+    private static int printSum(int n) {
+
+        if (n==0){
+            return 0;
+        }
+        return n+ printSum(n-1);
+
+    }
+}
