55b538994885850fc9a1132cd590d0860883f803
Nachiket <Sarafnachiket@gmail.com>
2023-01-19 00:10:44 +0530

    LeetCode problem 203 Remove Linked List Elements.

--- src/main/java/com/bootcoding/dsa/leetcode/linkedlist/RemoveLinkedListElements.java
+++ src/main/java/com/bootcoding/dsa/leetcode/linkedlist/RemoveLinkedListElements.java
@@ -8,30 +8,60 @@ public class RemoveLinkedListElements {
         ListNode firstnode = new ListNode(7);
         ListNode secondnode = new ListNode(7);
         ListNode thirdnode = new ListNode(7);
-        ListNode forthnode = new ListNode(7);
-        ListNode fifthnode = new ListNode(7);
-        ListNode sixthnode = new ListNode(7);
-        ListNode seventhnode = new ListNode(7);
+//        ListNode forthnode = new ListNode(3);
+//        ListNode fifthnode = new ListNode(4);
+//        ListNode sixthnode = new ListNode(5);
+//        ListNode seventhnode = new ListNode(6);
 
 
 
         ListNode head = firstnode;
         firstnode.next = secondnode;
         secondnode.next = thirdnode;
-        thirdnode.next = forthnode;
-        forthnode.next = fifthnode;
-        fifthnode.next= sixthnode;
-        sixthnode.next= seventhnode;
-        seventhnode.next = null;
+//        thirdnode.next = forthnode;
+//        forthnode.next = fifthnode;
+//        fifthnode.next= sixthnode;
+//        sixthnode.next= seventhnode;
+//        seventhnode.next = null;
         int val= 7;
-        ListNode h = removeElements(head,val);
+//        ListNode h = removeElements(head,val);
+        printList(head);
+        ListNode h = removeElements2(head,val);
+        printList(h);
 
+    }
+
+    public static void printList(ListNode head){
+        ListNode temp = head;
+        System.out.println();
+        while(temp != null){
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+    }
+    public static ListNode removeElements2(ListNode head, int val){
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp != null){
+            if(temp.val == val){
+                if(prev == null){
+                    head = temp.next;
+                }else{
+                    prev.next = temp.next;
+                }
+            }else {
+                prev = temp;
+            }
+            temp = temp.next;
+        }
+        return head;
     }
     public static ListNode removeElements(ListNode head, int val) {
         ListNode temp = head;
+        ListNode next = temp.next;
         while (temp!= null){
             if (temp.next.val==val){
-                if (temp.next.val == val && temp.next.next==null ){
+                if ( next.val == val && temp.next.next==null ){
                     temp.next=null;
                 }
                 System.out.println("val "+ temp.val);
