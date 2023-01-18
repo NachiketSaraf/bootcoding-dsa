8103591091973bc712ae68e2803a4099b5c18667
Nachiket <Sarafnachiket@gmail.com>
2023-01-19 00:12:44 +0530

    WAP to count the digits of a given number using recursion

---
+++ src/main/java/com/bootcoding/dsa/recursion/SumOfDigits.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.recursion;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int n = 123456;
+        int s = printDigitsSum(n);
+        System.out.println(s);
+    }
+
+    private static int printDigitsSum(int n) {
+        if (n<10){
+            return n;
+        }
+        return (n%10) +printDigitsSum(n/10);
+    }
+}
