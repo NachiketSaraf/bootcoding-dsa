b973623c2aa1f90ea6f00490424f3024aaac5dcf
Nachiket <Sarafnachiket@gmail.com>
2023-01-19 00:17:20 +0530

    WAP to calculate power of a number using recursion

---
+++ src/main/java/com/bootcoding/dsa/recursion/Power.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa.recursion;
+
+public class Power {
+    public static void main(String[] args) {
+        int n = 3;
+        int p = 4;
+        int c =calcPower(p,n);
+        System.out.println(c);
+
+    }
+
+    private static int calcPower(int p, int n) {
+
+        if (p==0){
+            return 1;
+        }
+        return n* calcPower(p-1,n);
+
+    }
+}
