49d45bb7feace56d6b38f51f4056c2c196beb115
Nachiket <Sarafnachiket@gmail.com>
2023-01-20 13:15:34 +0530

    LeetCode problem 258 Add Digits.

---
+++ src/main/java/com/bootcoding/dsa/leetcode/recursion/AddDigits.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.leetcode.recursion;
+
+public class AddDigits {
+    public static void main(String[] args) {
+        int n = 38;
+        int a= addDigits(n);
+        System.out.println(a);
+
+    }public static int addDigits(int num) {
+        if (num<10){
+            return num;
+        }
+        return addDigits(num/10 + num%10);
+
+    }
+}
