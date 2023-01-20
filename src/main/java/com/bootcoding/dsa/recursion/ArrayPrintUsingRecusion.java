3656de0a0f90086fccd050ae1533692d10537517
Nachiket <Sarafnachiket@gmail.com>
2023-01-20 11:31:20 +0530

    Print array using recursion

---
+++ src/main/java/com/bootcoding/dsa/recursion/ArrayPrintUsingRecusion.java
@@ -0,0 +1,22 @@
+package com.bootcoding.dsa.recursion;
+
+import java.util.Scanner;
+
+public class ArrayPrintUsingRecusion {
+
+
+    public static void main(String[] args) {
+        int[] array= {1,2,3,4,5};
+        getArray(array,0);
+    }
+
+    private static void getArray(int[] array,int i) {
+        if (i == array.length){
+            return;
+    }
+        System.out.println(array[i]);
+
+        getArray(array,i+1);
+    }
+
+}
