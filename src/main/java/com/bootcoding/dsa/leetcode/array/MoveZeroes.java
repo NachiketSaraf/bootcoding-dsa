5f730eeb3be6d8081aac3c4f8a31eab58249b93f
Nachiket <Sarafnachiket@gmail.com>
2023-01-20 12:45:20 +0530

    LeetCode Problem 283 Move Zeroes.

--- src/main/java/com/bootcoding/dsa/leetcode/array/MoveZeroes.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/MoveZeroes.java
@@ -3,20 +3,26 @@ package com.bootcoding.dsa.leetcode.array;
 public class MoveZeroes {
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
+        int[] a =  moveZeroes(nums);
+        for (int i = 0; i<= a.length;i++) {
+            System.out.println(a[i]);
+        }
 
     }
-    public static void moveZeroes(int[] nums) {
-        int temp ;
+    public static int[] moveZeroes(int[] nums) {
+        int temp = 0;
         for(int i = 0; i<nums.length;i++){
-            if(nums[i]==0){
-                temp = nums[i];
-
-
-                nums[nums.length-1]=nums[i];
-
-
+            if(nums[i]!=0) {
+                nums[temp]=nums[i];
+                temp++;
             }
         }
+        while (temp< nums.length)
+        {
+         nums[temp]=0;
+         temp++;
+        }
+        return nums;
 
     }
 }
