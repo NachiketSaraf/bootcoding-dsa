8da1aaa39d37f4ede33c3c498c0ce4eabb9199b3
Nachiket <Sarafnachiket@gmail.com>
2023-01-22 23:39:42 +0530

    Node class

---
+++ src/main/java/com/bootcoding/dsa/codingtestseries/Node.java
@@ -0,0 +1,11 @@
+package com.bootcoding.dsa.codingtestseries;
+
+public class Node {
+        int data;
+        Node next;
+
+        public Node(int d)
+        {
+            data = d;
+        }
+}
