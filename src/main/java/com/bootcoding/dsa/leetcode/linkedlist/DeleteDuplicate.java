a7e56a764dee1115a1c25ab27fff26c8349afe5e
Nachiket <Sarafnachiket@gmail.com>
2023-01-22 23:41:22 +0530

    delete duplicate in linked list

---
+++ src/main/java/com/bootcoding/dsa/leetcode/linkedlist/DeleteDuplicate.java
@@ -0,0 +1,27 @@
+//package com.bootcoding.dsa.leetcode.linkedlist;
+//
+//public class DeleteDuplicate {
+//    public static void main(String[] args) {
+//        ListNode firstnode = new ListNode(1);
+//        ListNode secondnode = new ListNode(1);
+//        ListNode thirdnode = new ListNode(2);
+////        ListNode forthnode = new ListNode(4);
+////        ListNode fifthnode = new ListNode(5);
+//
+//        ListNode head = firstnode;
+//        firstnode.next = secondnode;
+//        secondnode.next = thirdnode;
+////        thirdnode.next = forthnode;
+////        forthnode.next = fifthnode;
+////        fifthnode = null;
+//        ListNode h = deleteDuplicates(head);
+//        while (h != null) {
+//            System.out.println(h.val);
+//            h = h.next;
+//        }
+//    }
+//
+//    public static ListNode deleteDuplicates(ListNode head) {
+//
+//    }
+//}
