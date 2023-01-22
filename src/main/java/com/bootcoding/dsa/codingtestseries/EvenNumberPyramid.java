47b28e8221a1c8e88b6c1765953940de6762edfc
Nachiket <Sarafnachiket@gmail.com>
2023-01-22 23:30:14 +0530

    print even no pyramid

---
+++ src/main/java/com/bootcoding/dsa/codingtestseries/EvenNumberPyramid.java
@@ -0,0 +1,23 @@
+package com.bootcoding.dsa.codingtestseries;
+
+public class EvenNumberPyramid {
+    public static void main(String[] args) {
+        printEvenNumber();
+    }
+
+    private static void printEvenNumber() {
+        int row = 4;
+        int even = 0;
+        for (int i = 1 ; i<=row;i++){
+            for (int j = row-i;j>0;j--){
+                System.out.print("  ");
+            }
+            for (int j = 0;j<i;j++) {
+                even=even+2;
+                System.out.print("  "+even);
+            }
+            System.out.println();
+
+        }
+    }
+}
