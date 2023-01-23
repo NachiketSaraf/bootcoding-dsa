b072d1942cd9594c44c55e7fc62ce56f85587e22
Nachiket <Sarafnachiket@gmail.com>
2023-01-24 00:32:59 +0530

    LeetCode Problem 217. Contains Duplicate.

--- src/main/java/com/bootcoding/dsa/leetcode/array/ContainsDuplicate.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/ContainsDuplicate.java
@@ -1,18 +1,20 @@
 package com.bootcoding.dsa.leetcode.array;
 
+import java.util.Arrays;
+
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        int[] nums = {1,3,2};
         boolean n= containsDuplicate(nums);
         System.out.println(n);
 
     }
     public static boolean containsDuplicate(int[] nums) {
-        for (int i = 0;i<nums.length;i++){
-            for (int j = (i+1) ;j<nums.length;j++){
-                if (nums[i]==nums[j]){
-                    return true;
-                }
+        Arrays.sort(nums);
+
+        for (int i = 0;i<nums.length-1;i++){
+            if (nums[i]==nums[i+1]){
+                return true;
             }
         }
         return false;
