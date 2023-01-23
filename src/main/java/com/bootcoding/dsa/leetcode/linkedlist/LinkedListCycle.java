5a798eb6ac8a4347e7ab683a5498c6700150f056
Nachiket <Sarafnachiket@gmail.com>
2023-01-24 00:35:41 +0530

    LeetCode Problem 141. Linked List Cycle.

--- src/main/java/com/bootcoding/dsa/leetcode/linkedlist/LinkedListCycle.java
+++ src/main/java/com/bootcoding/dsa/leetcode/linkedlist/LinkedListCycle.java
@@ -1,5 +1,56 @@
 package com.bootcoding.dsa.leetcode.linkedlist;
 
 public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode firstnode = new ListNode(1);
+        ListNode secondnode = new ListNode(2);
+        ListNode thirdnode = new ListNode(3);
+        ListNode forthnode = new ListNode(4);
+        ListNode fifthnode = new ListNode(5);
+        ListNode sixthnode = new ListNode(6);
+        ListNode seventhnode = new ListNode(7);
+        ListNode eighthnode = new ListNode(8);
+        ListNode ninthnode = new ListNode(9);
+
+        ListNode head = firstnode;
+        firstnode.next = secondnode;
+        secondnode.next = thirdnode;
+        thirdnode.next = forthnode;
+        forthnode.next = fifthnode;
+        fifthnode.next = sixthnode;
+        sixthnode.next = seventhnode;
+        seventhnode.next = eighthnode;
+        eighthnode.next = ninthnode;
+        ninthnode.next = fifthnode;
+
+        findCycle(head);
+    }
+
+    private static void findCycle(ListNode head) {
+        ListNode fastPtr =head ;
+        ListNode slowPtr = head;
+
+        while (fastPtr == null || fastPtr.next==null){
+            if (fastPtr == null || fastPtr.next==null){
+                System.out.println("no cycle found");
+                break;
+            }
+            slowPtr=slowPtr.next;
+            fastPtr=fastPtr.next.next;
+            if (slowPtr==fastPtr){
+                System.out.println("There is a cycle in LL");
+                break;
+            }
+        }
+    }
+
+    public static void printList(ListNode head){
+        ListNode temp = head;
+        System.out.println();
+        while(temp != null){
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+    }
 
 }
