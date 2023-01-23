10ae888062fffa4b80e53493ca6cde1b3939eba9
Nachiket <Sarafnachiket@gmail.com>
2023-01-24 00:34:58 +0530

    LeetCode Problem 1295. Find Numbers with Even Number of Digits.

---
+++ src/main/java/com/bootcoding/dsa/leetcode/array/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,26 @@
+package com.bootcoding.dsa.leetcode.array;
+
+public class FindNumbersWithEvenNumberOfDigits {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+        int ec = findNumbers(nums);
+        System.out.println(ec);
+
+    } public static int findNumbers(int[] nums) {
+        int evenCounter = 0 ;
+        int div;
+        for (int i = 0;i < nums.length; i++){
+            int digitsCounter = 0;
+            int n = nums[i];
+            while (n!=0){
+                div = n/10;
+                n = div;
+                digitsCounter++;
+            }
+            if(digitsCounter%2 == 0){
+                evenCounter++;
+            }
+        }
+        return evenCounter;
+    }
+}
