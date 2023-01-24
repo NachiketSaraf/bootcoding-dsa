21f105a32b5c6db7c6989fd287bfd98092d3e0e6
Nachiket <Sarafnachiket@gmail.com>
2023-01-25 00:47:11 +0530

    LeetCode Problem 217. Contains Duplicate.

---
+++ src/main/java/com/bootcoding/dsa/hastable/DuplicateInArray.java
@@ -0,0 +1,30 @@
+package com.bootcoding.dsa.hastable;
+
+import java.util.HashMap;
+
+public class DuplicateInArray {
+    public static void main(String[] args) {
+            int a[] = {10, 20, 10, 30, 20, 40};
+            //find duplicate elements in an array
+
+
+            //<Integer ,Integer> generics
+            HashMap <Integer , Integer> store = new HashMap<>();
+            for(int i=0;i<a.length;i++){
+                if(store.containsKey(a[i])){
+                    int value = store.get(a[i]);
+                    store.put(a[i] ,value + 1);
+                }
+                else{
+                    store.put(a[i] ,1);
+                }
+            }
+            for(Object key : store.keySet()){
+                int value =  store.get(key); // no. of occurences)
+                if(value > 1){
+                    System.out.println(key);
+                }
+            }
+        }
+    }
+
