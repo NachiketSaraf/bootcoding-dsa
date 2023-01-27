7cf8588e34d4a6df6d01e819355db941a6fd49d2
Nachiket <Sarafnachiket@gmail.com>
2023-01-27 19:18:51 +0530

    LeetCode Problem 2239. Find Closest Number to Zero.

--- src/main/java/com/bootcoding/dsa/leetcode/array/FindClosestNumberToZero.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/FindClosestNumberToZero.java
@@ -1,5 +1,7 @@
 package com.bootcoding.dsa.leetcode.array;
 
+import java.util.Arrays;
+
 public class FindClosestNumberToZero {
     public static void main(String[] args) {
             int[] nums = {-10,-12,-54,-12,-544,-10000};
@@ -7,14 +9,26 @@ public class FindClosestNumberToZero {
         System.out.println(ans);
     }
     public static int findClosestNumber(int[] nums) {
-        int close = Math.abs(nums[nums.length-1]);
-        for (int i = 0; i < nums.length ; i++){
-            int sub = Math.abs(nums[i]);
-
-            if (sub < close){
-                close = sub;
+        Arrays.sort(nums);
+        int positive = 0;
+        int negative = nums[0];
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                positive = nums[i];
+                break;
             }
+            if(nums[i] == 0) {
+                return 0;
+            }
+            negative = nums[i];
         }
-        return close;
+        if(Math.abs(negative) < positive){
+            return negative;
+        }
+        if(positive == 0){
+            return nums[n - 1];
+        }
+        return positive;
     }
 }
