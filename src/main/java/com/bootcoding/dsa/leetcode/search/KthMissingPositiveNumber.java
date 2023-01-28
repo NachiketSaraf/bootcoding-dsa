d0c380774bfdcd0807795e854d8ad44c8b5054a5
Nachiket <Sarafnachiket@gmail.com>
2023-01-28 09:05:44 +0530

    LeetCode Problem 1539. Kth Missing Positive Number.

---
+++ src/main/java/com/bootcoding/dsa/leetcode/search/KthMissingPositiveNumber.java
@@ -0,0 +1,12 @@
+//package com.bootcoding.dsa.leetcode.search;
+//
+//import java.util.ArrayList;
+//
+//public class KthMissingPositiveNumber {
+//    public static void main(String[] args) {
+//        int[] arr = {2,3,4,7,11};
+//        System.out.println(findKthPositive(arr,5) );
+//    }public static int findKthPositive(int[] arr, int k) {
+//
+//    }
+//}
