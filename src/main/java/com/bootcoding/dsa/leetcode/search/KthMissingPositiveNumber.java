ac3e8021dd3df0acf3a1339d067bcefd8e5dceaf
Nachiket <Sarafnachiket@gmail.com>
2023-01-29 22:26:14 +0530

    1539. Kth Missing Positive Number

--- src/main/java/com/bootcoding/dsa/leetcode/search/KthMissingPositiveNumber.java
+++ src/main/java/com/bootcoding/dsa/leetcode/search/KthMissingPositiveNumber.java
@@ -1,12 +1,32 @@
-//package com.bootcoding.dsa.leetcode.search;
-//
-//import java.util.ArrayList;
-//
-//public class KthMissingPositiveNumber {
-//    public static void main(String[] args) {
-//        int[] arr = {2,3,4,7,11};
-//        System.out.println(findKthPositive(arr,5) );
-//    }public static int findKthPositive(int[] arr, int k) {
-//
+package com.bootcoding.dsa.leetcode.search;
+
+import java.util.ArrayList;
+
+public class KthMissingPositiveNumber {
+    public static void main(String[] args) {
+        int[] arr = {1,2};
+        System.out.println(findKthPositive(arr,1));
+    }
+    public static int findKthPositive(int[] arr, int k) {
+
+    }
+//     return binarySearch(arr,k,1,0,arr.length-1);
+//    public static int binarySearch(int a[] ,int x ,int i,int left, int right){
+//        int mid = (left + right) /2;
+//        if(i == a[mid]){
+//            return binarySearch(a,x ,++i,0 ,right);
+//        }
+//        if(x == 0){
+//            return i;
+//        }
+//        if(left >= right){
+//            return binarySearch(a,--x ,++i,0 ,a.length-1);
+//        }
+//        if(i>a[mid]){
+//            return binarySearch(a,x ,i,mid+1 ,right);
+//        }else{
+//            return binarySearch(a,x ,i,left ,mid-1);
+//        }
 //    }
-//}
+
+}
