a65f03748cc84ddcd9528372b23049f1cba15f29
Nachiket <Sarafnachiket@gmail.com>
2023-02-02 18:14:34 +0530

    LeetCode Problem 94. Binary Tree Inorder Traversal.

---
+++ src/main/java/com/bootcoding/dsa/leetcode/tree/InorderBinaryTree.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class InorderBinaryTree {
+    public static void main(String[] args) {
+
+    }
+    List<Integer> v = new LinkedList<>();
+    public  List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return v;
+        }
+        inorderTraversal(root.left);
+        v.add(root.val);
+        inorderTraversal(root.right);
+        return v;
+    }
+}
+
