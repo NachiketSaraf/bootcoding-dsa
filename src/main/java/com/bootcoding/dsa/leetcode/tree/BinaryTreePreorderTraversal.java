89c25f919b1ea9831561331cf22e5fcf21c1dacb
Nachiket <Sarafnachiket@gmail.com>
2023-02-02 18:20:50 +0530

    LeetCode Problem 144. Binary Tree Preorder Traversal.

---
+++ src/main/java/com/bootcoding/dsa/leetcode/tree/BinaryTreePreorderTraversal.java
@@ -0,0 +1,17 @@
+package com.bootcoding.dsa.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    List<Integer> values = new LinkedList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return values;
+        }
+        values.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return values;
+    }
+}
