af70fb6776b9e68764158dad62d15078bbd4e1c7
Nachiket <Sarafnachiket@gmail.com>
2023-02-18 02:21:29 +0530

    ArmstrongNumbers.

---
+++ src/main/java/com/bootcoding/dsa/gfg/ArmstrongNumbers.java
@@ -0,0 +1,27 @@
+package com.bootcoding.dsa.gfg;
+
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+
+    }
+    public static String armstrongNumber(int n){
+        int rem = 0;
+        int sum = 0;
+        int temp = n;
+        while(n != 0)
+        {
+            int div = n/10;
+            rem = n%10;
+            sum = sum + (rem*rem*rem );
+            n = div;
+        }
+        if ( sum == temp)
+        {
+            return  "Amstrong";
+        }
+        else
+        {
+            return  "Not Amstrong";
+        }
+    }
+}
