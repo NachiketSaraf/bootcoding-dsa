27d17410a262a6fcf8e269c5d5efcd757d66135a
Nachiket <Sarafnachiket@gmail.com>
2023-02-18 02:21:51 +0530

    Calculate Digits

--- src/main/java/com/bootcoding/dsa/recursion/CalculateDigits.java
+++ src/main/java/com/bootcoding/dsa/recursion/CalculateDigits.java
@@ -1,10 +1,17 @@
 package com.bootcoding.dsa.recursion;
 
+import static java.lang.Math.log10;
+
 public class CalculateDigits {
     public static void main(String[] args) {
         int a = 12345;
+        System.out.println(digtis(a));
         int count = calcDigits(a);
         System.out.println(count);
+    }
+    public static int digtis(int a){
+        int count = (int)(log10(a)+1);
+        return count;
     }
 
     private static int calcDigits(int a) {
