c8d63d68c9ea94e0b901ae4dead3759bf9a787c9
Nachiket <Sarafnachiket@gmail.com>
2023-03-20 19:16:00 +0530

    Leetcode Problem 136. Single Number

---
+++ src/main/java/com/bootcoding/dsa/leetcode/array/SingleNumber.java
@@ -0,0 +1,15 @@
+package com.bootcoding.dsa.leetcode.array;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] arr = {4,1,2,1,2};
+        System.out.println(singleNumber(arr));
+    }
+    public static int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int num:nums){
+            xor = xor^num;
+        }
+        return xor;
+    }
+}
