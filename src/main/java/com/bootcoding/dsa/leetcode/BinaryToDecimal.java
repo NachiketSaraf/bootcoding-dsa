a2ca3bf788735ad288c054163670533585e14c69
Nachiket <Sarafnachiket@gmail.com>
2023-03-27 00:42:46 +0530

    convert binary to decimal

---
+++ src/main/java/com/bootcoding/dsa/leetcode/BinaryToDecimal.java
@@ -0,0 +1,8 @@
+package com.bootcoding.dsa.leetcode;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binaryNumber = "1001";
+        System.out.println(Integer.parseInt(binaryNumber, 2));
+    }
+}
