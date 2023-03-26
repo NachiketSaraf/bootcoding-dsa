f7481dfe1ee5579233bcb75fab9457f6f5e2953a
Nachiket <Sarafnachiket@gmail.com>
2023-03-27 00:44:18 +0530

    find subset

--- src/main/java/com/bootcoding/dsa/leetcode/array/Subset.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/Subset.java
@@ -8,15 +8,15 @@ public class Subset {
        int[] a = {1,2,3};
        createAllSubArray(a);
     }
-//    public static List<List<Integer>> subsets(int[] nums) {
-//        List<List<Integer>> result = new ArrayList<>();
-//        for (int start =0 ; start<nums.length; start++){
-//            for (int end = start; end<nums.length; end++){
-//
-//            }
-//
-//        }
-//    }
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int start =0 ; start<nums.length; start++){
+            for (int end = start; end<nums.length; end++){
+
+            }
+
+        }
+    }
     private static void createAllSubArray(int[] arr) {
         // For creating the sub array
         for (int start =0 ; start<arr.length; start++){
