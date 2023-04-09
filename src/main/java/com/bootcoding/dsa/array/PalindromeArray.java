e56198fe1fc48c5e6ac2de2e8a72e1005047474a
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-09 22:27:26 +0530

    check whether array is palindrome or not

--- src/main/java/com/bootcoding/dsa/array/PalindromeArray.java
+++ src/main/java/com/bootcoding/dsa/array/PalindromeArray.java
@@ -6,12 +6,12 @@ public class PalindromeArray {
                 new int[]{11,2,2,1,1}));
     }
     public static String checkPalindrome(int[] arr){
-        int j = arr.length-1;
+        int m = arr.length-1;
         for (int i =0 ; i< arr.length/2;i++){
-            if (arr[i]!=arr[j]){
+            if (arr[i]!=arr[m]){
                 return "Array is not palindrome";
             }
-            j--;
+            m--;
         }
         return "Array is palindrome";
     }
