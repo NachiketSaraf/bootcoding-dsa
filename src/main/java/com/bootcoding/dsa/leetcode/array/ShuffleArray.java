fecb1b52ab7aea3f1e76caa5acba0ab73f20d435
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-10 22:17:55 +0530

    Leetcode problem 1470. Shuffle the Array

--- src/main/java/com/bootcoding/dsa/leetcode/array/ShuffleArray.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/ShuffleArray.java
@@ -3,8 +3,8 @@ package com.bootcoding.dsa.leetcode.array;
 public class ShuffleArray {
     public static void main(String[] args) {
         int[] res = shuffle(new int[]{2, 5, 1, 3, 4, 7}, 3);
-        for (int k = 0; k < res.length; k++) {
-            System.out.println(res[k]);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
         }
     }
     public static int[] shuffle(int[] nums, int n) {
