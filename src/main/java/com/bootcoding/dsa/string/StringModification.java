3d039dc5b9875b768736a49f3cc624c3baa98d6f
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-14 23:25:56 +0530

    string builder

---
+++ src/main/java/com/bootcoding/dsa/string/StringModification.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa.string;
+
+public class StringModification {
+    public static void main(String[] args) {
+        String s ="";
+        int n = 5;
+            StringBuilder sb = new StringBuilder();
+            sb.append("rollNO");
+            sb.append(",");
+            sb.append("cse");
+            sb.append(",");
+            sb.append("sub1");
+            sb.append(",");
+            sb.append("sub2");
+            sb.append(",");
+            sb.append("sub3");
+
+        System.out.println(sb);
+    }
+
+}
