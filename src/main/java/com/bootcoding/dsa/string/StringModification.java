ea409bf837124b5d9df4734205f51d826e2aff0a
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-14 23:45:43 +0530

    string builder

--- src/main/java/com/bootcoding/dsa/string/StringModification.java
+++ src/main/java/com/bootcoding/dsa/string/StringModification.java
@@ -4,17 +4,19 @@ public class StringModification {
     public static void main(String[] args) {
         String s ="";
         int n = 5;
-            StringBuilder sb = new StringBuilder();
-            sb.append("rollNO");
-            sb.append(",");
-            sb.append("cse");
-            sb.append(",");
-            sb.append("sub1");
-            sb.append(",");
-            sb.append("sub2");
-            sb.append(",");
-            sb.append("sub3");
+        StringBuilder sb = new StringBuilder();
+        sb.append("rollNO");
+        sb.append(",");
+        sb.append("cse");
+        sb.append(",");
+        sb.append("sub1");
 
+
+
+            sb.append(",");
+        sb.append("sub2");
+        sb.append(",");
+        sb.append("sub3");
         System.out.println(sb);
     }
 
