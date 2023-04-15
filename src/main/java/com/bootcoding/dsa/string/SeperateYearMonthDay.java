65b94f6eda0f052081d30f6c748a69f06ef14123
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-15 22:06:16 +0530

    Seperate Year Month Day

---
+++ src/main/java/com/bootcoding/dsa/string/SeperateYearMonthDay.java
@@ -0,0 +1,17 @@
+package com.bootcoding.dsa.string;
+
+public class SeperateYearMonthDay {
+    public static void main(String[] args) {
+        doTask("2023-04-14");
+    }
+    public static void doTask(String date){
+        String[] split = date.split("-");
+        String year = split[0];
+        String month = split[1];
+        String day = split[3];
+        System.out.println("Year"+year);
+        System.out.println("Month"+month);
+        System.out.println("Day"+day);
+
+    }
+}
