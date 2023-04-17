6cb60e21a17c62685977e89901feace42483843b
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-17 15:30:23 +0530

    Leetcode problem 1662. Check If Two String Arrays are Equivalent

---
+++ src/main/java/com/bootcoding/dsa/leetcode/string/CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa.leetcode.string;
+
+public class CheckIfTwoStringArraysAreEquivalent {
+    public static void main(String[] args) {
+        String[]  word1 = {"ab", "c"};
+        String[]  word2 = {"a", "bc"};
+        System.out.println(arrayStringsAreEqual(word1,word2));
+    }
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        if (word1==null || word2==null) return false;
+        String word1Sum =null;
+        String word2Sum =null;
+        for (int i = 0; i < word1.length ; i++) {
+            word1Sum += word1[i];
+        }
+        for (int i = 0; i < word2.length ; i++) {
+            word2Sum += word2[i];
+        }
+        return word1Sum.equals(word2Sum);
+    }
+}
