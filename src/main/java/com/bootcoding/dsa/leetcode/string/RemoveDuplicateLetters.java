edc0b11ccfcd049eabd5d00a9d71395ee1d4ae9e
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-18 23:20:53 +0530

    Leetcode problem 316. Remove Duplicate Letters

--- src/main/java/com/bootcoding/dsa/leetcode/string/RemoveDuplicateLetters.java
+++ src/main/java/com/bootcoding/dsa/leetcode/string/RemoveDuplicateLetters.java
@@ -4,18 +4,14 @@ import java.util.*;
 
 public class RemoveDuplicateLetters {
     public static void main(String[] args) {
-        String s = "bcdbc";
-        System.out.println(removeDuplicateLetters(s));
+        //String s = "bcdbc";
+        System.out.println(removeDuplicateLetters("bcdbc"));
     }
     public static String removeDuplicateLetters(String s) {
         StringBuilder sb = new StringBuilder();
-
         HashSet<Character> set =new HashSet<>();
-
         for (int i = 0; i < s.length(); i++) set.add(s.charAt(i));
-
         for (char ch: set) sb.append(ch);
-
         return sb.toString();
     }
 }
