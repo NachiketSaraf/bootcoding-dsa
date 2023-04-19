047ceef692ddbfc6bab132b842dbc47597d39900
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-19 15:11:34 +0530

    Copy the even element from given array to new array

--- src/main/java/com/bootcoding/dsa/array/CopyEven.java
+++ src/main/java/com/bootcoding/dsa/array/CopyEven.java
@@ -6,9 +6,7 @@ public class CopyEven {
     public static void main(String[] args) {
         int[] arr = {14,35,66,75,92,3,5,8,7,9};
         int[] res = copyEvenInNewArray(arr);
-        for (int re : res) {
-            System.out.println(re);
-        }
+        for (int re : res) System.out.println(re);
 
     }
 
