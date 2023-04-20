b54469d43085c2b0bbe7a15c3d31679be92f172e
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-20 23:58:44 +0530

    CubeOfArray

--- src/main/java/com/bootcoding/dsa/array/CubeOfArray.java
+++ src/main/java/com/bootcoding/dsa/array/CubeOfArray.java
@@ -5,12 +5,9 @@ import java.util.Arrays;
 public class CubeOfArray {
     public static void main(String[] args) {
         int[] cube = Cube(new int[]{1,2,-3});
-
         for (int j : cube) System.out.println(j);
     }
     public static int[] Cube(int[] array){
-
         return Arrays.stream(array).map(j -> j * j * j).toArray();
-
     }
 }
