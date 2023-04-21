d1a4cf5793e27df0f46d7f2f7dfba186adf210c7
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-22 00:36:35 +0530

    Leetcode problem 2325. Decode the Message

--- src/main/java/com/bootcoding/dsa/leetcode/hashtable/DecodeTheMessage.java
+++ src/main/java/com/bootcoding/dsa/leetcode/hashtable/DecodeTheMessage.java
@@ -7,7 +7,7 @@ public class DecodeTheMessage {
     public static void main(String[] args) {
         String key = "the quick brown fox jumps over the lazy dog";
         String message = "vkbs bs t suepuv";
-        System.out.println(decodeMessage("the quick brown fox jumps over the lazy dog","vkbs bs t suepuv"));
+        System.out.println(decodeMessage(key,"vkbs bs t suepuv"));
     }
     public static String decodeMessage(String key, String message) {
         StringBuilder sb = new StringBuilder();
