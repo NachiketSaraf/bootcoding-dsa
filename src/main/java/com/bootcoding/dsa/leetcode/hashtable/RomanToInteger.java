236923e8dfafd2da9a5be95576a892cb1ccd9487
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-23 00:28:24 +0530

    Leetcode problem 13. Roman to Integer

--- src/main/java/com/bootcoding/dsa/leetcode/hashtable/RomanToInteger.java
+++ src/main/java/com/bootcoding/dsa/leetcode/hashtable/RomanToInteger.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 public class RomanToInteger {
     public static void main(String[] args) {
-        String s = "LVIII";
         System.out.println(romanToInt("MCMXCIV"));
     }
     public static int romanToInt(String s) {
