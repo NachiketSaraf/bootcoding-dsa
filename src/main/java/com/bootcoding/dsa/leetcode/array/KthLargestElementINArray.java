72da5ccd03cfd0d5e275403b707842115adc12e2
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-25 23:11:21 +0530

    Leetcode problem 215. Kth Largest Element in an Array

--- src/main/java/com/bootcoding/dsa/leetcode/array/KthLargestElementINArray.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/KthLargestElementINArray.java
@@ -9,7 +9,7 @@ public class KthLargestElementINArray {
 
     public static int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
-        var n = nums[nums.length - k];
+        int n = nums[nums.length - k];
         return n;
     }
 
