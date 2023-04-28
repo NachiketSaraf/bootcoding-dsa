42e164c96dd3f9c65a68dc4f409932addbdb176e
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-28 22:49:34 +0530

    Bubble Sort

--- src/main/java/com/bootcoding/dsa/sort/BubbleSort.java
+++ src/main/java/com/bootcoding/dsa/sort/BubbleSort.java
@@ -8,17 +8,16 @@ public class BubbleSort {
             System.out.println(s[i]);
         }
     }
-
-    private static int[] sortBubble(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length-1; j++) {
-                if (numbers[j] > numbers[j+1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
+    private static int[] sortBubble(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num.length-1; j++) {
+                if (num[j] > num[j+1]) {
+                    int temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
                 }
             }
         }
-        return numbers;
+        return num;
     }
 }
