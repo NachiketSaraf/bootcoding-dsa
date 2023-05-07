717ca870894800a55b20e2197f6771c77be250e3
NachiketSaraf <sarafnachiket@gmail.com>
2023-05-08 01:17:09 +0530

    EvenNumberPyramid

--- src/main/java/com/bootcoding/dsa/codingtestseries/EvenNumberPyramid.java
+++ src/main/java/com/bootcoding/dsa/codingtestseries/EvenNumberPyramid.java
@@ -9,9 +9,7 @@ public class EvenNumberPyramid {
         int row = 4;
         int even = 0;
         for (int i = 1 ; i<=row;i++){
-            for (int j = row-i;j>0;j--){
-                System.out.print("  ");
-            }
+            for (int j = row-i;j>0;j--) System.out.print("  ");
             for (int j = 0;j<i;j++) {
                 even=even+2;
                 System.out.print("  "+even);
