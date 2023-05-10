236632d182553b09b35e26c52a2e36001aca847a
NachiketSaraf <sarafnachiket@gmail.com>
2023-05-10 21:28:15 +0530

    Array Print Using Recursion

--- src/main/java/com/bootcoding/dsa/recursion/ArrayPrintUsingRecusion.java
+++ src/main/java/com/bootcoding/dsa/recursion/ArrayPrintUsingRecusion.java
@@ -3,8 +3,8 @@ package com.bootcoding.dsa.recursion;
 public class ArrayPrintUsingRecusion {
 
     public static void main(String[] args) {
-        int[] array= {1,2,3,4,5};
-        getArray(array,0);
+        int[] array= ;
+        getArray(new int[] {1,2,3,4,5},0);
     }
     private static void getArray(int[] array,int k) {
         if (k == array.length) return;
