53a53b5368e533fd94e4eb71d78944a83d5f86a6
NachiketSaraf <sarafnachiket@gmail.com>
2023-05-12 22:28:29 +0530

    412. Fizz Buzz

--- src/main/java/com/bootcoding/dsa/leetcode/FizzBuzz.java
+++ src/main/java/com/bootcoding/dsa/leetcode/FizzBuzz.java
@@ -19,9 +19,8 @@ public class FizzBuzz {
 
             if (divisibleBy3 && divisibleBy5) ans.add("FizzBuzz");
             else if (divisibleBy3) ans.add("Fizz");
-            else if (divisibleBy5) {
-                ans.add("Buzz");
-            } else {
+            else if (divisibleBy5) ans.add("Buzz");
+            else {
                 ans.add(Integer.toString(num));
             }
         }
