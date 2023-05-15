7ca2065a590d6aecbf5d311a17b1989b03be6ed5
NachiketSaraf <sarafnachiket@gmail.com>
2023-05-16 00:22:23 +0530

    1137. N-th Tribonacci Number

--- src/main/java/com/bootcoding/dsa/leetcode/hashtable/TribonacciNumber.java
+++ src/main/java/com/bootcoding/dsa/leetcode/hashtable/TribonacciNumber.java
@@ -10,9 +10,7 @@ public class TribonacciNumber {
     static HashMap<Integer, Integer> cache = new HashMap<>();
     public static int tribonacci(int n) {
         if (n == 0) return 0;
-        if(n == 1 || n ==2){
-            return 1;
-        }
+        if(n == 1 || n ==2) return 1;
         if (cache.containsKey(n)) {
             return cache.get(n);
         }
