b56d61baea7559f05da9c7aec5278f57442192a2
NachiketSaraf <sarafnachiket@gmail.com>
2023-07-01 19:59:10 +0530

    creating and printing linked list

---
+++ src/main/java/com/bootcoding/dsa/linkedlist/Create.java
@@ -0,0 +1,26 @@
+package com.bootcoding.dsa.linkedlist;
+
+public class Create {
+    public static void main(String[] args) {
+
+        Node firstNode = new Node(1);
+        Node secondNode = new Node(2);
+        Node thirdNode = new Node(3);
+        Node forthNode = new Node(4);
+
+
+        Node head = firstNode;
+        firstNode.next= secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = forthNode;
+        forthNode.next = null;
+
+        Node temp = head;
+
+        while (temp!= null){
+            System.out.println(temp.data);
+            temp= temp.next;
+        }
+
+    }
+}
