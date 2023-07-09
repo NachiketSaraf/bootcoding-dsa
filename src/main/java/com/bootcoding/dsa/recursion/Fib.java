c4598285cfe18b004caee853bbbab61c36fd3131
NachiketSaraf <sarafnachiket@gmail.com>
2023-07-09 23:59:09 +0530

    fib using memoization

---
+++ src/main/java/com/bootcoding/dsa/recursion/Fib.java
@@ -0,0 +1,30 @@
+package com.bootcoding.dsa.recursion;
+
+import com.bootcoding.dsa.leetcode.recursion.Fibonacci;
+
+import java.util.HashMap;
+
+public class Fib {
+    public static void main(String[] args) {
+            Fib f = new Fib();
+           long b = f.fib(6015);
+            System.out.println(b);
+        }
+        HashMap<Long,Long> map = new HashMap<>();
+        public long fib(long n){
+            if(n==0) {
+                return 0;
+            }
+            if (n==1){
+                return 1;
+            }
+            if (map.containsKey(n)){
+                return map.get(n);
+            }
+            map.put(n,fib(n-1) + fib(n-2));
+            return map.get(n);
+        }
+
+    }
+
+
