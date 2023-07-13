0ca18b3d26063542c573a05208d2ba8be121602a
NachiketSaraf <sarafnachiket@gmail.com>
2023-07-14 02:18:24 +0530

    111. Minimum Depth of Binary Tree

---
+++ src/main/java/com/bootcoding/dsa/leetcode/binerytree/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,18 @@
+package com.bootcoding.dsa.leetcode.binerytree;
+
+public class MinimumDepthOfBinaryTree {
+    public static void main(String[] args) {
+
+    }
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        if (root.left==null && root.right==null){
+            return 1;
+        }
+        if (root.left==null || root.right==null){
+            return 1+Math.max(minDepth(root.left),minDepth(root.right));
+        }
+        return 1+Math.min(minDepth(root.left),minDepth(root.right));
+    }
+}
