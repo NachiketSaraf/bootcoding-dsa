850fb58f2a42f8dffce8b7090f9a9f60e8874a6f
NachiketSaraf <sarafnachiket@gmail.com>
2023-07-20 23:31:28 +0530

    ZigZag

---
+++ src/main/java/com/bootcoding/dsa/binarytree/ZigZag.java
@@ -0,0 +1,70 @@
+package com.bootcoding.dsa.binarytree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ZigZag {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n9 = new TreeNode(9);
+        TreeNode n10 = new TreeNode(10);
+        TreeNode n11 = new TreeNode(11);
+        TreeNode n12 = new TreeNode(12);
+        TreeNode n13 = new TreeNode(13);
+        TreeNode n14 = new TreeNode(14);
+        TreeNode n15 = new TreeNode(15);
+        TreeNode n16 = new TreeNode(16);
+        TreeNode n17 = new TreeNode(17);
+
+        root.left = n2;
+        root.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n4.left = n8;
+        n4.right = n9;
+        n5.left = n10;
+        n5.right = n11;
+        n6.left = n12;
+        n6.right = n13;
+        n7.left = n14;
+        n7.right = n15;
+        n8.left = n16;
+        n8.right = n17;
+
+        zigZaglevelOrder(root);
+    }
+    public static void zigZaglevelOrder(TreeNode root){
+        if (root == null) return;
+        Queue<TreeNode> queue =new ArrayDeque<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i=0 ; i<size ; i++){
+               TreeNode node = queue.peek();
+               queue.poll();
+
+           //    int index = (leftToRight) ? i:(size-1-i);
+
+//               if(node.left){
+//                   queue.add(node.left);
+//               }
+//               if(node.right){
+//
+//               }
+            }
+        }
+    }
+}
