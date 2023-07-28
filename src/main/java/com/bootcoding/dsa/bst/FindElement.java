ee10b5a14895ddbbfc1821a79b5717fab102328d
NachiketSaraf <sarafnachiket@gmail.com>
2023-07-29 01:45:10 +0530

    Find Element in BST

---
+++ src/main/java/com/bootcoding/dsa/bst/FindElement.java
@@ -0,0 +1,26 @@
+package com.bootcoding.dsa.bst;
+
+public class FindElement {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        TreeNode n2 = new TreeNode(5);
+        TreeNode n3 = new TreeNode(16);
+        TreeNode n4 = new TreeNode(14);
+        TreeNode n5 = new TreeNode(15);
+        TreeNode n6 = new TreeNode(20);
+
+        root.left = n2;
+        root.right = n3;
+        n3.left = n4;
+        n3.right = n6;
+        n4.right = n5;
+
+        System.out.println(search(root,10));
+    }
+
+    private static boolean search(TreeNode root , int target) {
+        if (root== null) return false;
+        if (root.data == target) return true;
+        return  (target< root.data)? search(root.left,target):search(root.right,target);
+    }
+}
