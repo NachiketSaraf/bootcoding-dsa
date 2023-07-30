7683a3b79372c2303e1169d76ee66e848fd22e9f
NachiketSaraf <sarafnachiket@gmail.com>
2023-07-31 01:31:09 +0530

    Inorder

---
+++ src/main/java/com/bootcoding/dsa/bst/Inorder.java
@@ -0,0 +1,28 @@
+package com.bootcoding.dsa.bst;
+
+public class Inorder {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        TreeNode n2 = new TreeNode(5);
+        TreeNode n3 = new TreeNode(16);
+        TreeNode n4 = new TreeNode(14);
+        TreeNode n5 = new TreeNode(15);
+        TreeNode n6 = new TreeNode(20);
+
+        root.left = n2;
+        root.right = n3;
+        n3.left = n4;
+        n3.right = n6;
+        n4.right = n5;
+
+        inorder(root);
+    }
+    public static void inorder(TreeNode root){
+        if (root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
+}
