47015e000bebdc8f86ad4897d7d11b6bf040d4f2
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-01 02:06:17 +0530

    Level order in binary tree

---
+++ src/main/java/com/bootcoding/dsa/binarytree/LevelOrder.java
@@ -0,0 +1,51 @@
+package com.bootcoding.dsa.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(8);
+
+        root.left = n2;
+        root.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n4.left = n8;
+
+        List<List<Integer>> ans = levelOrder(root);
+        for (List<Integer> n: ans) System.out.println(n);
+    }
+
+    private static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            ArrayList<Integer> subArray = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                if (queue.peek().left != null) queue.offer(queue.peek().left);
+                if (queue.peek().right != null) queue.offer(queue.peek().right);
+                subArray.add(queue.poll().data);
+            }
+            ans.add(subArray);
+        }
+        return ans;
+    }
+}
