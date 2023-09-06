0cb1f3a2ece1e71a3d94463cbc7dbb5abba295e2
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-06 22:27:47 +0530

    String Example

---
+++ src/main/java/com/bootcoding/dsa/string/StringExample.java
@@ -0,0 +1,10 @@
+package com.bootcoding.dsa.string;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String s = "java";
+        System.out.println(s);
+        s="java8";
+        System.out.println(s);
+    }
+}
