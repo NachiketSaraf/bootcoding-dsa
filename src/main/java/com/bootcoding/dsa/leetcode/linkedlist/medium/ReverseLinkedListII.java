136d2b7bc38dda6f47698c4fdfb123744c739da3
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-08 00:35:04 +0530

    92. Reverse Linked List II

--- src/main/java/com/bootcoding/dsa/leetcode/linkedlist/medium/ReverseLinkedListII.java
+++ src/main/java/com/bootcoding/dsa/leetcode/linkedlist/medium/ReverseLinkedListII.java
@@ -16,28 +16,53 @@ public class ReverseLinkedListII {
         secondnode.next = thirdnode;
         thirdnode.next = forthnode;
         forthnode.next = fifthnode;
-        fifthnode.next= sixthnode;
-        sixthnode.next= seventhnode;
+        fifthnode.next = sixthnode;
+        sixthnode.next = seventhnode;
         seventhnode.next = null;
 
-        ListNode newHead = reverseBetween(firstnode, 2, 5);
-        while (newHead != null){
+        ListNode newHead = reverseBetweenOptimized(firstnode, 2, 5);
+        while (newHead != null) {
             System.out.println(newHead.val);
-            newHead= newHead.next;
+            newHead = newHead.next;
         }
     }
+
+    public static ListNode reverseBetweenOptimized(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+        ListNode dummy = new ListNode(0);
+        ListNode prev = null;
+        ListNode tail = null;
+
+        dummy.next = head;
+        prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        tail = prev.next;
+
+        ListNode temp = null;
+        for (int i = 0; i < (right-left); i++) {
+            temp = prev.next;
+            prev.next = tail.next;
+            tail.next = tail.next.next;
+            prev.next.next = temp;
+        }
+        return dummy.next;
+    }
+
     public static ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode current = head;
         ListNode prev = null;
 
         int count = 1;
-        while (count != left){
+        while (count != left) {
             prev = current;
             current = current.next;
             count++;
         }
         ListNode start = current;
-        while (count != right){
+        while (count != right) {
             current = current.next;
             count++;
         }
@@ -45,25 +70,26 @@ public class ReverseLinkedListII {
         current.next = null;
 
         ListNode newHead = reverseList(start);
-        if (prev != null){
+        if (prev != null) {
             prev.next = newHead;
         }
         current = newHead;
-        while (current.next != null){
+        while (current.next != null) {
             current = current.next;
         }
         current.next = rest;
-        if (left == 1){
+        if (left == 1) {
             return newHead;
         }
         return head;
     }
+
     public static ListNode reverseList(ListNode head) {
         ListNode current = head;
         ListNode next = null;
         ListNode prev = null;
 
-        while (current != null){
+        while (current != null) {
             next = current.next;
             current.next = prev;
             prev = current;
