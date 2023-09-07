b9fe3de06193a4f5610b7c5d7af9531189ee6070
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-07 22:41:32 +0530

    list node class with random pointer

--- src/main/java/com/bootcoding/dsa/leetcode/linkedlist/ListNode.java
+++ src/main/java/com/bootcoding/dsa/leetcode/linkedlist/ListNode.java
@@ -2,8 +2,8 @@ package com.bootcoding.dsa.leetcode.linkedlist;
 
 public class ListNode
 {
-      int val;
-      ListNode next;
+      public int val;
+      public ListNode next;
       public ListNode(int val){
 
           this.val = val;
