c8f73ceb0e0107890dae5af1c8bab4179648a44b
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-19 02:28:03 +0530

    Compare The Triplets

---
+++ src/main/java/com/bootcoding/dsa/hackerrank/CompareTheTriplets.java
@@ -0,0 +1,29 @@
+package com.bootcoding.dsa.hackerrank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CompareTheTriplets {
+    public static void main(String[] args) {
+
+    }
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        int aa = 0;
+        int bb = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) >b.get(i))  aa++;
+            else if (b.get(i)>a.get(i)) bb++;
+
+        }
+        ans.add(aa);
+        ans.add(bb);
+
+        return ans;
+    }
+}
