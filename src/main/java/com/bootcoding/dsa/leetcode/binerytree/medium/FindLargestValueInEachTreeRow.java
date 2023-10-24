307873c776a4906e286f7e5f7f92d4ce425fd8cc
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-24 16:43:29 +0530

    515. Find Largest Value in Each Tree Row

---
+++ src/main/java/com/bootcoding/dsa/leetcode/binerytree/medium/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,42 @@
+package com.bootcoding.dsa.leetcode.binerytree.medium;
+
+import com.bootcoding.dsa.binarytree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindLargestValueInEachTreeRow {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int max_val = Integer.MIN_VALUE;
+            int levelSize = q.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = q.poll();
+                max_val = Math.max(max_val, node.val);
+
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+
+            res.add(max_val);
+        }
+
+        return res;
+    }
+}
