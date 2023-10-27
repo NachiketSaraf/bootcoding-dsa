6e6f9383f455e260210b80b51459b34bec26a2fc
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-28 00:59:54 +0530

    5. Longest Palindromic Substring

--- src/main/java/com/bootcoding/dsa/leetcode/string/medium/LongestPalindromicSubstring.java
+++ src/main/java/com/bootcoding/dsa/leetcode/string/medium/LongestPalindromicSubstring.java
@@ -2,7 +2,7 @@ package com.bootcoding.dsa.leetcode.string.medium;
 
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-
+        String s = "babad";
     }
     public String longestPalindrome(String s) {
         StringBuilder stringBuilder = new StringBuilder("^#");
