fd9fd7a98a6cb6c246c7a48ced533a3adb815ee2
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-31 02:47:42 +0530

    1356. Sort Integers by The Number of 1 Bits

--- src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/SortIntegersByTheNumberOf1Bits.java
+++ src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/SortIntegersByTheNumberOf1Bits.java
@@ -10,10 +10,12 @@ public class SortIntegersByTheNumberOf1Bits {
         }
     }
     public static int[] sortByBits(int[] arr) {
+        // Using stream to copy a array and change from primitive to Wrapper class
         Integer[] boxedArr =  Arrays.stream(arr)
                 .boxed()
                 .toArray(Integer[]::new);
 
+        //Normal way to copy a array
 //        Integer[] boxedArr = new Integer[arr.length];
 //        for (int i = 0; i < arr.length; i++) {
 //            boxedArr[i] = arr[i];
