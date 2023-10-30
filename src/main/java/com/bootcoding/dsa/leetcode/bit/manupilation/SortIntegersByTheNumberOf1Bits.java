eb8226ebb979af1375b4a5d8faf2a53f61731679
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-31 02:21:39 +0530

    1356. Sort Integers by The Number of 1 Bits

--- src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/SortIntegersByTheNumberOf1Bits.java
+++ src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/SortIntegersByTheNumberOf1Bits.java
@@ -8,76 +8,29 @@ public class SortIntegersByTheNumberOf1Bits {
             System.out.println(i);
         }
     }
-    /***************************************************************** JAVA *****************************************************************/
-//Approach-1 (Using self written countBits function inside lambda)
-//T.C : O(n*logn * log(num))
-//The countOneBits loop iterates for the number of bits in the input number, which is log₂(n)
-//sort takes nlogn
-
-        public static int countOneBits(int num) {
-            int count = 0;
-            while (num != 0) {
-                count += num & 1;
-                num >>= 1;
-            }
-            return count;
+    public static int[] sortByBits(int[] arr) {
+        Integer[] boxedArr = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            boxedArr[i] = arr[i];
         }
 
-        public static int[] sortByBitsLambda(int[] arr) {
-            Integer[] boxedArr = new Integer[arr.length];
-            for (int i = 0; i < arr.length; i++) {
-                boxedArr[i] = arr[i];
+        Arrays.sort(boxedArr, (a, b) -> {
+            int countA = Integer.bitCount(a);
+            int countB = Integer.bitCount(b);
+
+            if (countA == countB) {
+                return a.compareTo(b);
             }
 
-            Arrays.sort(boxedArr, (a, b) -> {
-                int countA = countOneBits(a);
-                int countB = countOneBits(b);
+            return Integer.compare(countA, countB);
+        });
 
-                if (countA == countB) {
-                    return a.compareTo(b);
-                }
-
-                return Integer.compare(countA, countB);
-            });
-
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = boxedArr[i];
-            }
-
-            return arr;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = boxedArr[i];
         }
 
-
-
-//Approach-2 (using inbuilt function)
-//T.C : Integer.bitCount calculates the number of set bits (1 bits) in an integer in constant time, not depending on the size of the integer.
-//So, overall T.C : O(n log n) by sorting
-
-
-
-        public static int[] sortByBits(int[] arr) {
-            Integer[] boxedArr = new Integer[arr.length];
-            for (int i = 0; i < arr.length; i++) {
-                boxedArr[i] = arr[i];
-            }
-
-            Arrays.sort(boxedArr, (a, b) -> {
-                int countA = Integer.bitCount(a);
-                int countB = Integer.bitCount(b);
-
-                if (countA == countB) {
-                    return a.compareTo(b);
-                }
-
-                return Integer.compare(countA, countB);
-            });
-
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = boxedArr[i];
-            }
-
-            return arr;
-        }
+        return arr;
+    }
 
 }
 
