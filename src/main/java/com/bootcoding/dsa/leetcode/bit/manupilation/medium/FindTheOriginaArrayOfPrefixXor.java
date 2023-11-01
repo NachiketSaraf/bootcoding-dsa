16aee2661cde14d5a3b50bdeb86c786fbc91f85f
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-01 23:30:26 +0530

    2433. Find The Original Array of Prefix Xor

--- src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/medium/FindTheOriginaArrayOfPrefixXor.java
+++ src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/medium/FindTheOriginaArrayOfPrefixXor.java
@@ -2,7 +2,7 @@ package com.bootcoding.dsa.leetcode.bit.manupilation.medium;
 
 public class FindTheOriginaArrayOfPrefixXor {
     public static void main(String[] args) {
-
+       int[] pref = {5,2,0,3,1};
     }
     public int[] findArray(int[] pref) {
         int[] arr = new int[pref.length];
