59e240a339276f231d4bc2ba60234ed2cc60314f
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-06 01:40:05 +0530

    1535. Find the Winner of an Array Game

--- src/main/java/com/bootcoding/dsa/leetcode/array/medium/FindTheWinnerOfAnArrayGame.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/medium/FindTheWinnerOfAnArrayGame.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class FindTheWinnerOfAnArrayGame {
     public static void main(String[] args) {
         int[] arr = {2,1,3,5,4,6,7};
-
+        getWinner(arr,2);
     }
     public static int getWinner(int[] arr, int k) {
         int n =arr.length;
