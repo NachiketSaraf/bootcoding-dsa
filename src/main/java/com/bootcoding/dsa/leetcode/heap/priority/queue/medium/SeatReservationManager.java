815eb05a269a134cb549bfd491df0bbe11a2b3d3
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-07 02:19:31 +0530

    1845. Seat Reservation Manager

--- src/main/java/com/bootcoding/dsa/leetcode/heap/priority/queue/medium/SeatReservationManager.java
+++ src/main/java/com/bootcoding/dsa/leetcode/heap/priority/queue/medium/SeatReservationManager.java
@@ -3,8 +3,19 @@ package com.bootcoding.dsa.leetcode.heap.priority.queue.medium;
 import java.util.PriorityQueue;
 
 public class SeatReservationManager {
-
-    class SeatManager {
+    /**
+     * Your SeatManager object will be instantiated and called as such:
+     * SeatManager obj = new SeatManager(n);
+     * int param_1 = obj.reserve();
+     * obj.unreserve(seatNumber);
+     */
+    public static void main(String[] args) {
+        SeatManager seatManager = new SeatManager(5);
+        int param_1 = seatManager.reserve();
+        seatManager.unreserve(seatNumber);
+    }
+}
+class SeatManager {
     PriorityQueue<Integer> pq = new PriorityQueue<>() ;
     int seat;
     public SeatManager(int n) {
@@ -22,15 +33,5 @@ public class SeatReservationManager {
 
     public void unreserve(int seatNumber) {
         pq.add(seatNumber);
-    }
-}
-    /**
-     * Your SeatManager object will be instantiated and called as such:
-     * SeatManager obj = new SeatManager(n);
-     * int param_1 = obj.reserve();
-     * obj.unreserve(seatNumber);
-     */
-    public static void main(String[] args) {
-        SeatManager
     }
 }
