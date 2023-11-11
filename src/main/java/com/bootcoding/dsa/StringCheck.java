155dd7d6e52a563be2f91aae0c652f9ac62a56af
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-11 23:22:45 +0530

    StringCheck

--- src/main/java/com/bootcoding/dsa/StringCheck.java
+++ src/main/java/com/bootcoding/dsa/StringCheck.java
@@ -2,7 +2,7 @@ package com.bootcoding.dsa;
 
 public class StringCheck {
     public static void main(String[] args) {
-//        System.out.println(solution("AGTCTCTATAAAG" ));
+        System.out.println(solution("AGTCTCTATAAAG" ));
         for (int i = 0; i < 100; i++) {
             System.out.println(i);
             System.out.println(solve(i));
