4211f335168c9298cef048168abb6e83ef4d749a
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-14 23:57:01 +0530

    SortIntegersByTheNumberOf1Bits

--- src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/SortIntegersByTheNumberOf1Bits.java
+++ src/main/java/com/bootcoding/dsa/leetcode/bit/manupilation/SortIntegersByTheNumberOf1Bits.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class SortIntegersByTheNumberOf1Bits {
     public static void main(String[] args) {
         int[] arr = {0,1,2,3,4,5,6,7,8};
-        int[] ans = sortByBits(arr);
+        int[] ans = sortByBits(new int[]{0,1,2,3,4,5,6,7,8});
         for (int i : ans){
             System.out.println(i);
         }
