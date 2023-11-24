b8e3f29a16da2fd06511712417687052d8bb40d3
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-24 23:54:43 +0530

    FindElementIn2DArray

--- src/main/java/com/bootcoding/dsa/matrix/FindElementIn2DArray.java
+++ src/main/java/com/bootcoding/dsa/matrix/FindElementIn2DArray.java
@@ -4,9 +4,9 @@ public class FindElementIn2DArray {
     public static void main(String[] args) {
         int[][] arr = new int[1][2];
         arr[0][0] = 1; arr[0][1] = 3;
-//        arr[0][2] = 2;
-//        arr[1][0] = 4; arr[1][1] = 5; arr[1][2] = 6;
-//        arr[2][0] = 7; arr[2][1] = 8; arr[2][2] = 9;
+        arr[0][2] = 2;
+        arr[1][0] = 4; arr[1][1] = 5; arr[1][2] = 6;
+        arr[2][0] = 7; arr[2][1] = 8; arr[2][2] = 9;
 
         findElement(arr,3);
     }
