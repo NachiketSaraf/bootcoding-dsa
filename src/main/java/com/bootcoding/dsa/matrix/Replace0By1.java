f307876ba6315bcb7ee452227391e4fce3e3af2b
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-25 23:17:31 +0530

    Replace0By1

--- src/main/java/com/bootcoding/dsa/matrix/Replace0By1.java
+++ src/main/java/com/bootcoding/dsa/matrix/Replace0By1.java
@@ -6,7 +6,6 @@ public class Replace0By1 {
         matrix[0][0] = 1; matrix[0][1] = 2; matrix[0][2] = 0;
         matrix[1][0] = 0; matrix[1][1] = 5; matrix[1][2] = 6;
         matrix[2][0] = 7; matrix[2][1] = 0; matrix[2][2] = 9;
-
         replace(matrix);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
