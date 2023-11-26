42fbf2c9040b2956d7e3a70eae7611385a2222cc
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-26 23:23:04 +0530

    twoSum

--- src/main/java/com/bootcoding/dsa/leetcode/hashtable/TwoSum.java
+++ src/main/java/com/bootcoding/dsa/leetcode/hashtable/TwoSum.java
@@ -6,7 +6,7 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] nums={2,7,11,15};
         int[] ans = twoSum(nums,9);
-        for (int i = 0; i < indx.length; i++ ){
+        for (int i = 0; i < ans.length; i++ ){
             System.out.println(indx[i]);
         }
     }
