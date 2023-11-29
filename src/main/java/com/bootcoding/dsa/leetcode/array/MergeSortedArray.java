4891a9c89c778d02d255edde3cdeb008348616e0
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-29 23:44:27 +0530

    Merge Sorted Array

--- src/main/java/com/bootcoding/dsa/leetcode/array/MergeSortedArray.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/MergeSortedArray.java
@@ -4,6 +4,7 @@ public class MergeSortedArray {
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
+
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m-1;
