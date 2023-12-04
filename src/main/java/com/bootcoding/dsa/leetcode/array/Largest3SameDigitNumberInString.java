238c8cfb0de1d5f94b3bcbc9862897cff793bc36
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-04 21:18:49 +0530

    2264. Largest 3-Same-Digit Number in String

--- src/main/java/com/bootcoding/dsa/leetcode/array/Largest3SameDigitNumberInString.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/Largest3SameDigitNumberInString.java
@@ -2,4 +2,14 @@ package com.bootcoding.dsa.leetcode.array;
 
 public class Largest3SameDigitNumberInString {
 
+    public String largestGoodInteger(String num) {
+        char maxDigit = '\0';
+        for (int index = 0; index <= num.length() - 3; ++index) {
+            if (num.charAt(index) == num.charAt(index + 1) && num.charAt(index) == num.charAt(index + 2)) {
+                maxDigit = (char) Math.max(maxDigit, num.charAt(index));
+            }
+        }
+        return maxDigit == '\0' ? "" : new String(new char[]{maxDigit, maxDigit, maxDigit});
+    }
+
 }
