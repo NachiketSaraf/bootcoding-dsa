ecf53e59b1608c9fe4dddfa5f44eb383cb6e11c4
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-06 23:20:51 +0530

    Count of Matches in Tournament

--- src/main/java/com/bootcoding/dsa/leetcode/array/CountOfMatchesInTournament.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/CountOfMatchesInTournament.java
@@ -4,4 +4,17 @@ public class CountOfMatchesInTournament {
     public static void main(String[] args) {
 
     }
+    public int numberOfMatches(int n) {
+        int ans = 0;
+        while (n > 1) {
+            if (n % 2 == 0) {
+                ans += n / 2;
+                n = n / 2;
+            } else {
+                ans += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        return ans;
+    }
 }
