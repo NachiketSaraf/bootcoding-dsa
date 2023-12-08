6102e288c4e721b4f7425e9bb6646e10ab41dd2b
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-08 23:07:55 +0530

    ...

--- src/main/java/com/bootcoding/dsa/leetcode/array/CountOfMatchesInTournament.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/CountOfMatchesInTournament.java
@@ -2,7 +2,7 @@ package com.bootcoding.dsa.leetcode.array;
 
 public class CountOfMatchesInTournament {
     public static void main(String[] args) {
-        System.out.println(numberOfMatches(7));
+        System.out.println(numberOfMatches(59));
     }
     public static int numberOfMatches(int n) {
         int ans = 0;
