ff0b1e0b2671f6c3a098edc9f4727d8c0f08d953
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-11 22:53:35 +0530

    867. Transpose Matrix

--- src/main/java/com/bootcoding/dsa/leetcode/matrix/TransposeMatrix.java
+++ src/main/java/com/bootcoding/dsa/leetcode/matrix/TransposeMatrix.java
@@ -2,7 +2,7 @@ package com.bootcoding.dsa.leetcode.matrix;
 
 public class TransposeMatrix {
     public static void main(String[] args) {
-
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
     }
     public int[][] transpose(int[][] matrix) {
         int row = matrix.length;
