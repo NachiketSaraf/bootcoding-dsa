5fbb6763d045f4b6a79e6f698e5649aa388f7415
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-14 00:00:58 +0530

    Special Positions in a Binary Matrix

--- src/main/java/com/bootcoding/dsa/leetcode/array/SpecialPositionsInABinaryMatrix.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/SpecialPositionsInABinaryMatrix.java
@@ -1,40 +1,39 @@
 package com.bootcoding.dsa.leetcode.array;
 
 public class SpecialPositionsInABinaryMatrix {
-    class Solution {
-        public int numSpecial(int[][] mat) {
-            int ans = 0;
-            int m = mat.length;
-            int n = mat[0].length;
 
-            for (int row = 0; row < m; row++) {
-                for (int col = 0; col < n; col++) {
-                    if (mat[row][col] == 0) {
-                        continue;
-                    }
+    public static int numSpecial(int[][] mat) {
+        int ans = 0;
+        int m = mat.length;
+        int n = mat[0].length;
 
-                    boolean good = true;
-                    for (int r = 0; r < m; r++) {
-                        if (r != row && mat[r][col] == 1) {
-                            good = false;
-                            break;
-                        }
-                    }
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] == 0) {
+                    continue;
+                }
 
-                    for (int c = 0; c < n; c++) {
-                        if (c != col && mat[row][c] == 1) {
-                            good = false;
-                            break;
-                        }
-                    }
-
-                    if (good) {
-                        ans++;
+                boolean good = true;
+                for (int r = 0; r < m; r++) {
+                    if (r != row && mat[r][col] == 1) {
+                        good = false;
+                        break;
                     }
                 }
-            }
 
-            return ans;
+                for (int c = 0; c < n; c++) {
+                    if (c != col && mat[row][c] == 1) {
+                        good = false;
+                        break;
+                    }
+                }
+
+                if (good) {
+                    ans++;
+                }
+            }
         }
+
+        return ans;
     }
 }
