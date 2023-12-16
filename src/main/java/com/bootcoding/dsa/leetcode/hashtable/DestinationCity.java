691c82c853b8e8c6d2df1329ccc69526c5810631
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-16 23:48:20 +0530

    1436. Destination City

--- src/main/java/com/bootcoding/dsa/leetcode/hashtable/DestinationCity.java
+++ src/main/java/com/bootcoding/dsa/leetcode/hashtable/DestinationCity.java
@@ -1,6 +1,9 @@
 package com.bootcoding.dsa.leetcode.hashtable;
 
 public class DestinationCity {
+    public static void main(String[] args) {
+
+    }
     public String destCity(List<List<String>> paths) {
         Set<String> set = new HashSet();
         for (int i = 0; i < paths.size(); i++) set.add(paths.get(i).get(0));
