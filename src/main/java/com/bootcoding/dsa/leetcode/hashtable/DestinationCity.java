65766750dd251e0fec4ac8ec915d307d8f51c054
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-20 23:48:03 +0530

    661. Image Smoother

--- src/main/java/com/bootcoding/dsa/leetcode/hashtable/DestinationCity.java
+++ src/main/java/com/bootcoding/dsa/leetcode/hashtable/DestinationCity.java
@@ -1,28 +1,28 @@
-package com.bootcoding.dsa.leetcode.hashtable;
-
-import java.util.*;
-
-public class DestinationCity {
-    public static void main(String[] args) {
-        List<List<String>> paths = new ArrayList<>();
-        List<String> innerList1 = new ArrayList<>("London , New York");
-        List<String> innerList = new ArrayList<>("London , New York");
-
-        innerList1.add(0,"London ");
-        innerList1.add(1, ", New York");
-        innerList.add(1,"New York , Lima");
-        innerList.add(2,"Lima , Sao Paulo");
-
-        paths.add(innerList);
-        System.out.println(destCity(paths));
-    }
-    public static String destCity(List<List<String>> paths) {
-        Set<String> set = new HashSet();
-        for (int i = 0; i < paths.size(); i++) set.add(paths.get(i).get(0));
-        for (int i = 0; i < paths.size(); i++) {
-            String candidate = paths.get(i).get(1);
-            if (!set.contains(candidate)) return candidate;
-        }
-        return "";
-    }
-}
+//package com.bootcoding.dsa.leetcode.hashtable;
+//
+//import java.util.*;
+//
+//public class DestinationCity {
+//    public static void main(String[] args) {
+//        List<List<String>> paths = new ArrayList<>();
+//        List<String> innerList1 = new ArrayList<>("London , New York");
+//        List<String> innerList = new ArrayList<>("London , New York");
+//
+//        innerList1.add(0,"London ");
+//        innerList1.add(1, ", New York");
+//        innerList.add(1,"New York , Lima");
+//        innerList.add(2,"Lima , Sao Paulo");
+//
+//        paths.add(innerList);
+//        System.out.println(destCity(paths));
+//    }
+//    public static String destCity(List<List<String>> paths) {
+//        Set<String> set = new HashSet();
+//        for (int i = 0; i < paths.size(); i++) set.add(paths.get(i).get(0));
+//        for (int i = 0; i < paths.size(); i++) {
+//            String candidate = paths.get(i).get(1);
+//            if (!set.contains(candidate)) return candidate;
+//        }
+//        return "";
+//    }
+//}
