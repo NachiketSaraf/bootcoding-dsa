18e2d8cf48002ea8ad0df146a53abd9c2386b550
NachiketSaraf <sarafnachiket@gmail.com>
2023-12-22 23:59:43 +0530

    661. Image Smoother

--- src/main/java/com/bootcoding/dsa/leetcode/matrix/ImageSmoother.java
+++ src/main/java/com/bootcoding/dsa/leetcode/matrix/ImageSmoother.java
@@ -14,6 +14,7 @@ public class ImageSmoother {
             System.out.println();
         }
     }
+
     private static final int[][] directions = {
             {-1, -1}, {-1, 0}, {-1, 1},
             {0, -1}, {0, 0}, {0, 1},
