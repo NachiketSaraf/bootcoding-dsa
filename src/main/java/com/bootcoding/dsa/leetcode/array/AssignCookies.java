95b62b85ecfed4dfa397fea4f045f1eae3ee1d1f
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-01 23:42:06 +0530

    455. Assign Cookies

--- src/main/java/com/bootcoding/dsa/leetcode/array/AssignCookies.java
+++ src/main/java/com/bootcoding/dsa/leetcode/array/AssignCookies.java
@@ -1,4 +1,17 @@
 package com.bootcoding.dsa.leetcode.array;
 
 public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int contentChildren = 0;
+        int cookieIndex = 0;
+        while (cookieIndex < s.length && contentChildren < g.length) {
+            if (s[cookieIndex] >= g[contentChildren]) {
+                contentChildren++;
+            }
+            cookieIndex++;
+        }
+        return contentChildren;
+    }
 }
