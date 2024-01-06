573f0a3aa63e045cf8e7f1319363a75a3d93736a
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-06 23:43:37 +0530

    MinimumNumberOfOperationsToMakeArrayEmpty

--- src/main/java/com/bootcoding/dsa/leetcode/greedy/medium/MinimumNumberOfOperationsToMakeArrayEmpty.java
+++ src/main/java/com/bootcoding/dsa/leetcode/greedy/medium/MinimumNumberOfOperationsToMakeArrayEmpty.java
@@ -5,16 +5,17 @@ import java.util.Map;
 
 public class MinimumNumberOfOperationsToMakeArrayEmpty {
     public static void main(String[] args) {
-        int[] nums = {2,3,3,2,2,4,2,3,4};
+        int[] nums = {2, 3, 3, 2, 2, 4, 2, 3, 4};
         System.out.println(minOperations(nums));
     }
+
     public static int minOperations(int[] nums) {
-        int count=0;
-        Map<Integer,Integer> freq= new HashMap<>();
-        for(int n : nums){
-            freq.put(n,freq.getOrDefault(n,0)+1);
+        int count = 0;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int n : nums) {
+            freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
-        for(int f:freq.values()){
+        for (int f : freq.values()) {
             if (f == 1) {
                 return -1;
             }
