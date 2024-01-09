c51629d4d8a4bbeb4e2b42792416b2edef2e86ae
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-09 15:04:12 +0530

    872. Leaf-Similar Trees

--- src/main/java/com/bootcoding/dsa/leetcode/binerytree/LeafSimilarTrees.java
+++ src/main/java/com/bootcoding/dsa/leetcode/binerytree/LeafSimilarTrees.java
@@ -1,5 +1,21 @@
 package com.bootcoding.dsa.leetcode.binerytree;
 
 public class LeafSimilarTrees {
-
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> firstTreeLeaves = new ArrayList<>();
+        List<Integer> secondTreeLeaves = new ArrayList<>();
+        preOrder(root1,firstTreeLeaves);
+        preOrder(root2,secondTreeLeaves);
+        return firstTreeLeaves.equals(secondTreeLeaves);
+    }
+    public void preOrder(TreeNode root,List<Integer> treeLeavesList){
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right == null){
+            treeLeavesList.add(root.val);
+        }
+        preOrder(root.left,treeLeavesList);
+        preOrder(root.right,treeLeavesList);
+    }
 }
