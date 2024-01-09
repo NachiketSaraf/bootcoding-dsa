f2e9765156e5e48514a729f74b0bc2f3fbe5218d
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-09 15:14:45 +0530

    872. Leaf-Similar Trees

--- src/main/java/com/bootcoding/dsa/leetcode/binerytree/LeafSimilarTrees.java
+++ src/main/java/com/bootcoding/dsa/leetcode/binerytree/LeafSimilarTrees.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class LeafSimilarTrees {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
+        TreeNode root2 = new TreeNode(3);
         TreeNode n1 = new TreeNode(5);
         TreeNode n2 = new TreeNode(1);
         TreeNode n3 = new TreeNode(6);
@@ -14,7 +15,22 @@ public class LeafSimilarTrees {
         TreeNode n7 = new TreeNode(7);
         TreeNode n8 = new TreeNode(4);
 
-        System.out.println(leafSimilar(root,root));
+
+        root.left=n1;
+        root.right=n2;
+        n1.left=n3;
+        n1.right=n4;
+        n2.left=n5;
+        n2.right=n6;
+        n4.left=n7;
+        n4.right=n8;
+
+        root2.left=n8;
+        root2.right=n2;
+        n2.left=n5;
+        n2.right=n6;
+
+        System.out.println(leafSimilar(root,root2));
     }
     public static boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> firstTreeLeaves = new ArrayList<>();
