d8efe4e009a2148a0828afe91360df07e13352a3
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-10 23:57:25 +0530

    2385. Amount of Time for Binary Tree to Be Infected

--- src/main/java/com/bootcoding/dsa/leetcode/binerytree/medium/AmountOfTimeForBinaryTreeToBeInfected.java
+++ src/main/java/com/bootcoding/dsa/leetcode/binerytree/medium/AmountOfTimeForBinaryTreeToBeInfected.java
@@ -1,5 +1,34 @@
 package com.bootcoding.dsa.leetcode.binerytree.medium;
 
-public class AmountOfTimeForBinaryTreeToBeInfected {
+import com.bootcoding.dsa.leetcode.binerytree.TreeNode;
 
+public class AmountOfTimeForBinaryTreeToBeInfected {
+    private int maxDistance = 0;
+    public int amountOfTime(TreeNode root, int start) {
+        traverse(root, start);
+        return maxDistance;
+    }
+
+    public int traverse(TreeNode root, int start) {
+        int depth = 0;
+        if (root == null) {
+            return depth;
+        }
+
+        int leftDepth = traverse(root.left, start);
+        int rightDepth = traverse(root.right, start);
+
+        if (root.val == start) {
+            maxDistance = Math.max(leftDepth, rightDepth);
+            depth = -1;
+        } else if (leftDepth >= 0 && rightDepth >= 0) {
+            depth = Math.max(leftDepth, rightDepth) + 1;
+        } else {
+            int distance = Math.abs(leftDepth) + Math.abs(rightDepth);
+            maxDistance = Math.max(maxDistance, distance);
+            depth = Math.min(leftDepth, rightDepth) - 1;
+        }
+
+        return depth;
+    }
 }
