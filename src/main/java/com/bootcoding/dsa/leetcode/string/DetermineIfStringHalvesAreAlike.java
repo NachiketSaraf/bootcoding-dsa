2332e3c3eab19559769516b29eec95eec24507a0
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-12 15:19:35 +0530

    Determine if String Halves Are Alike

---
+++ src/main/java/com/bootcoding/dsa/leetcode/string/DetermineIfStringHalvesAreAlike.java
@@ -0,0 +1,26 @@
+package com.bootcoding.dsa.leetcode.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DetermineIfStringHalvesAreAlike {
+    public boolean halvesAreAlike(String s) {
+        int aCount = 0;
+        int bCount = 0;
+
+        Set<Character> vowels = new HashSet<>();
+        String vowelsString = "aeiouAEIOU";
+        for(char c: vowelsString.toCharArray()) vowels.add(c);
+
+        int i=0;
+        int j = s.length()/2;
+        while (i < s.length()/2 && j < s.length() ) {
+            if (vowels.contains(s.charAt(i))) aCount++;
+            if (vowels.contains(s.charAt(j))) bCount++;
+            i++;
+            j++;
+        }
+
+        return aCount == bCount;
+    }
+}
