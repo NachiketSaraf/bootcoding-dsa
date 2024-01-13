9230d9583371b22ccf42d7c7b239c44b9ebbc3ec
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-13 23:43:14 +0530

    Minimum Number of Steps to Make Two Strings Anagram

--- src/main/java/com/bootcoding/dsa/leetcode/string/medium/MinimumNumberOfStepsToMakeTwoStringsAnagram.java
+++ src/main/java/com/bootcoding/dsa/leetcode/string/medium/MinimumNumberOfStepsToMakeTwoStringsAnagram.java
@@ -1,5 +1,22 @@
 package com.bootcoding.dsa.leetcode.string.medium;
 
 public class MinimumNumberOfStepsToMakeTwoStringsAnagram {
+    public int minSteps(String s, String t) {
+        int[] count = new int[26];
+        // Storing the difference of frequencies of characters in t and s.
+        for (int i = 0; i < s.length(); i++) {
+            count[t.charAt(i) - 'a']++;
+            count[s.charAt(i) - 'a']--;
+        }
 
+        int ans = 0;
+        // Adding the difference where string t has more instances than s.
+        // Ignoring where t has fewer instances as they are redundant and
+        // can be covered by the first case.
+        for (int i = 0; i < 26; i++) {
+            ans += Math.max(0, count[i]);
+        }
+
+        return ans;
+    }
 }
