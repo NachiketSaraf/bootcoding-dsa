29cf407d4311dc0f58b2426a6e8919dc24a46ef9
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-14 23:45:15 +0530

    1657. Determine if Two Strings Are Close

--- src/main/java/com/bootcoding/dsa/leetcode/string/medium/DetermineIfTwoStringsAreClose.java
+++ src/main/java/com/bootcoding/dsa/leetcode/string/medium/DetermineIfTwoStringsAreClose.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class DetermineIfTwoStringsAreClose {
     public static void main(String[] args) {
-        closeStrings("abc","bca");
+        System.out.println(closeStrings("abc","bca"));
     }
 
     public static boolean closeStrings(String word1, String word2) {
