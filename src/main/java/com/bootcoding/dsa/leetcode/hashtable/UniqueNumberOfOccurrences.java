5340b804ca17ccfbb516c0cce5fe0147efd95879
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-17 18:55:56 +0530

    Unique Number of Occurrences

---
+++ src/main/java/com/bootcoding/dsa/leetcode/hashtable/UniqueNumberOfOccurrences.java
@@ -0,0 +1,23 @@
+package com.bootcoding.dsa.leetcode.hashtable;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> freq = new HashMap<>();
+
+        for (int n:arr) {
+            freq.put(n, freq.getOrDefault(n,0)+1);
+        }
+
+        Set<Integer> check = new HashSet<>();
+        for (int n : freq.values()){
+            check.add(n);
+        }
+
+        return freq.size() == check.size();
+    }
+}
