c274b1d8a0c28b5a5ead5508c5eb8439f0cf5b1e
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-18 16:50:18 +0530

    Climbing Stairs

--- src/main/java/com/bootcoding/dsa/leetcode/ClimbingStairs.java
+++ src/main/java/com/bootcoding/dsa/leetcode/ClimbingStairs.java
@@ -10,9 +10,7 @@ public class ClimbingStairs {
     }
     static HashMap<Integer, Integer> cache = new HashMap<>();
     public static int climbStairs(int n) {
-        if(n==0 || n==1){
-            return 1;
-        }
+        if(n==0 || n==1) return 1;
         if (cache.containsKey(n)) return cache.get(n);
         cache.put(n,climbStairs(n-1)+climbStairs(n-2));
         return cache.get(n);
