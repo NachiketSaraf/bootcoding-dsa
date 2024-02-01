4755b3bbe9ce5a050eb151895bb58b8ecce4b8dc
NachiketSaraf <sarafnachiket@gmail.com>
2024-02-01 23:57:44 +0530

    Divide Array Into Arrays With Max Difference

--- src/main/java/com/bootcoding/dsa/leetcode/greedy/medium/DivideArrayIntoArraysWithMaxDifference.java
+++ src/main/java/com/bootcoding/dsa/leetcode/greedy/medium/DivideArrayIntoArraysWithMaxDifference.java
@@ -3,13 +3,15 @@ package com.bootcoding.dsa.leetcode.greedy.medium;
 import java.util.*;
 public class DivideArrayIntoArraysWithMaxDifference {
 
-    Arrays.sort(nums);
-    int[][] ans = new int[nums.length / 3][3];
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int[][] ans = new int[nums.length / 3][3];
         for (int i = 0; i < nums.length; i += 3) {
-        if (nums[i + 2] - nums[i] > k) {
-            return new int[0][0];
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][0];
+            }
+            ans[i / 3] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
         }
-        ans[i / 3] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
-    }
         return ans;
+    }
 }
