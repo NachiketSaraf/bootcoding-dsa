ff25311b502851df53a9204bfca9534d57a5ba3e
NachiketSaraf <sarafnachiket@gmail.com>
2024-02-03 23:58:20 +0530

    1043. Partition Array for Maximum Sum

--- src/main/java/com/bootcoding/dsa/leetcode/dp/medium/PartitionArrayForMaximumSum.java
+++ src/main/java/com/bootcoding/dsa/leetcode/dp/medium/PartitionArrayForMaximumSum.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class PartitionArrayForMaximumSum {
     public static void main(String[] args) {
         int[] arr = {1,15,7,9,2,5,10};
-        maxSumAfterPartitioning(arr,3);
+        System.out.println(maxSumAfterPartitioning(arr,3));
     }
     private static int maxSum(int[] arr, int k, int[] dp, int start) {
         int N = arr.length;
